6031c12bdd09cd72d0810e29388d472b33712c2e
grmkris <kristjan.grm1@gmail.com>
2022-03-29 23:01:22 +0200

    feat: added new function definitions

--- index.ts
+++ index.ts
@@ -1,11 +1,68 @@
 import axios from 'axios';
+// TODO https://github.com/pseudozach/boltz-frontend/blob/b472f3223067cfdbaacad4a3db7c3855dd0b90ba/src/actions/swapActions.js#L258-L258
 
 const API_URL: string = 'https://api.marduk.exchange:9001';
 
-export function getRates(): Promise<object> {
+export const getRates = (): Promise<object> =>  {
   return new Promise((resolve, reject) => {
     axios
       .get(`${API_URL}/getpairs`)
+      .then((resp) => {
+        resolve(resp.data);
+      })
+      .catch(reject);
+  });
+}
+
+
+export const startReverseSwap = () :  Promise<object> =>  {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${API_URL}/startreverse`)
+      .then((resp) => {
+        resolve(resp.data);
+      })
+      .catch(reject);
+  });
+}
+
+export const broadcastClaimTransaction = (currency: string, claimTransaction: string, cb: string) :  Promise<object> =>  {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${API_URL}/broadcasttransaction`)
+      .then((resp) => {
+        resolve(resp.data);
+      })
+      .catch(reject);
+  });
+}
+
+export const createSwap = (currency: string, amount: string, cb: string) :  Promise<object> =>  {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${API_URL}/createswap`)
+      .then((resp) => {
+        resolve(resp.data);
+      })
+      .catch(reject);
+  });
+}
+
+export const swapStatus = (currency: string, cb: string) :  Promise<object> =>  {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${API_URL}/swapstatus`)
+      .then((resp) => {
+        resolve(resp.data);
+      })
+      .catch(reject);
+  });
+}
+
+export const getFeeEstimation = (currency: string, amount: string, cb: string) :  Promise<object> =>  {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${API_URL}/getfeeestimation`)
       .then((resp) => {
         resolve(resp.data);
       })
---
+++ readme.md
@@ -0,0 +1,2 @@
+https://github.com/MardukExchange/marduk-sdk
+
